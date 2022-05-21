ae1bbaaa1be8bbb7a5fc505484c04f669d2252c5
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-20 22:39:44 -0400

    refactor: renamed toronto to washington usa

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn app() -> Html {
     html! {
         <div>
             <h1>{"Weather Application"}</h1>
-            <h3>{"The current weather for Toronto is:"}</h3>
+            <h3>{"The current weather for Washington, USA is:"}</h3>
             <p>{"It is currently sunny"}</p>
         </div>
     }
